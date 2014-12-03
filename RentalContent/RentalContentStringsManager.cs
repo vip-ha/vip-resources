7e65a4d58b22055033186d3cb86c27c895470754
Darren Haligas <dahaligas@homeaway.com>
2014-12-03 10:45:04 -0600

    initial commit

---
+++ RentalContent/RentalContentStringsManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladToHaveYou.Resources.RentalContent
+{
+    public class RentalContentTexts
+    {
+        public string Title { get; set; }
+        public string HelpText { get; set; }
+        public string Keywords { get; set; }
+    }
+        
+    public class RentalContentStringsManager
+    {
+        public static RentalContentTexts GetText(String tag)
+        {
+            var key = tag.Replace(".", "_");
+
+            return new RentalContentTexts()
+            {
+                Title = RentalContentStrings.ResourceManager.GetString(key + "_title"),
+                HelpText = RentalContentStrings.ResourceManager.GetString(key + "_helptext"),
+                Keywords = RentalContentStrings.ResourceManager.GetString(key + "_keywords"),
+            };
+        }
+    }
+}
